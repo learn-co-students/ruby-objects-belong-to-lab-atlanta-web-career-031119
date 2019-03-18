835ac4c6e6a3b96cb69612366bb347589e0e1648
github username <github email address>
2019-03-18 14:08:01 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,8 @@
 require 'pry'
 
 def class artist
-    def initialize(name, song_name)
+    def initialize(name)
         @name=name
-        @song=song_name
     end
-    attr_reader :name, :song
+    attr_accessor :name
 end
